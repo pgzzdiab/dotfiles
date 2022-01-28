8294c3362945804a38e5db109282cbfc54b3e8d7
pierre <pierre.gauthier@diabeloop.fr>
2022-01-28 09:46:56 +0100

    script to auto tmux sessions creaion

---
+++ .config/tmux/auto_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Session Name
+session=$(git branch --show-current)
+SESSIONEXISTS=$(tmux list-sessions | grep $session)
+
+# Only create tmux session if it doesn't already exist
+if not [ "$SESSIONEXISTS" = "" ]
+then
+  # tmux kill-session -t $session
+  tmux a -t $session
+fi
+
+# Start New Session with our name
+tmux new-session -d -s $session
+
+tmux rename-window -t $session:1 -n 'neovim'
+tmux send-keys -t 'neovim' C-m 'nvim' C-m
+
+# Create and setup pane
+tmux new-window -t $session:2 -n 'python'
+# tmux rename-window -t $session:1 -n 'python'
+tmux send-keys -t 'python' C-m '. venv/bin/activate' C-m
+
+# Setup an additional shell
+tmux new-window -t $session:3 -n 'ranger'
+tmux send-keys -t 'ranger' C-m "ranger" C-m
+
+
+# Attach Session, on the Main window
+tmux attach-session -t $session:1
